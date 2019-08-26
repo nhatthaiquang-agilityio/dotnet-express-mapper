78d25c287f65d980d8db30c66e90961f2c993772
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2019-08-26 08:25:47 +0700

    Add APIs

--- dotnet-express-mapper/Data/AppDbContext.cs
+++ dotnet-express-mapper/Data/AppDbContext.cs
@@ -38,5 +38,6 @@ namespace dotnet_express_mapper.Data
         public DbSet<Book> Books { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Author> Authors { get; set; }
+        public DbSet<BookCategory> BookCategories { get; set; }
     }
 }
